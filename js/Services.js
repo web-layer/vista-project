2b443be0e61bb66f7b8244306ee118e2ec2bc7cb
yassin belmaya <ebelmaya.yassin2019@gmail.com>
2024-12-09 08:21:54 +0100

    add pro

---
+++ js/Services.js
@@ -0,0 +1,54 @@
+JSON.parse(localStorage.getItem('prodacts'))
+
+let prodact = [
+    {
+        id:1,
+        title: "pmv chamara",
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        price: 590,
+        img: 'pmv-chamara.jpg',
+    },
+
+    {
+        id:2,
+        title: "smart watch",
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        price: 50,
+        img: 'chuttersnap.jpg',
+    },
+
+    {
+        id:3,
+        title: "pmv chamara",
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        price: 80,
+        img: 'Shoes.jpg',
+    },
+
+    {
+        id:4,
+        title: "Bluetooth Headphones",
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        price: 100,
+        img: 'Headphones.jpg',
+    },
+
+    {
+        id:5,
+        title: "wristwatch",
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        price: 120,
+        img: 'wristwatch.jpg',
+    },
+
+    {
+        id:6,
+        title: "Fashion Watch",
+        discription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+        price: 170,
+        img: 'Fashion-Watch.jpg',
+    },
+]
+
+
+export  default prodact
