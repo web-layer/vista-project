44afc827291840342bdc3683b42b51d90e749769
yassin belmaya <ebelmaya.yassin2019@gmail.com>
2025-02-09 15:56:38 +0100

    qpdate file css html

--- js/app.js
+++ js/app.js
@@ -28,66 +28,22 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("Offrons").scrollIntoView({ behavior: "smooth" });
     }
 
-
-
-
-
-
-
-
-
-
-
-    /*         
-        let send = document.querySelector(".send")
-    
-        send.addEventListener("click", function () {
-    
-            let input = document.querySelector("textarea")
-    
-            let getinputvalue = input.value.trim()
-    
-            let phone = '+212 661-522768'
-    
-            let massge = `https://api.whatsapp.com/send?phone=${phone}&text=${getinputvalue}`;
-    
-            window.open(massge, "_blank")
-    
-        }) */
-
-
-
-
 })
 
 
 
+ScrollReveal({
+    reast: true,
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
 
-/* 
-    let form = document.getElementById('googleForm').addEventListener('submit', function (e) {
-        const email = encodeURIComponent(document.getElementById('email').value);
-        const name = encodeURIComponent(document.getElementById('name').value);
-    
-    
-    
-        fetch('https://docs.google.com/forms/u/0/d/e/1FAIpQLScvFpIyMloB4JAEY-HCSdnV0I5K3wSkY11vLOafJfQAh7flmQ/formResponse', {
-            method: 'POST',
-            mode: 'no-cors',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: `entry.1243685087=${email}&entry.1353529448=${name}`
-        })
-            .then(() => {
-                console.log('Form submitted successfully!');
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-                console('Failed to submit the form.');
-            });
-    
-    })
- */
+})
+
+ScrollReveal().reveal('.info');
+ScrollReveal().reveal('.Services');
+
+
 
 
 
